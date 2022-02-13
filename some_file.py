4a459003b347cb25a019a16d0be958c5c8213013
Sebastian Kupisiewicz <sebastian.kupisiewicz1@gmail.com>
2022-02-13 12:53:35 +0100

    add some file

---
+++ some_file.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+
+def do_something(data):
+
+    if not data.empty:
+        print("DataFrame detected")
+        return np.mean(data['col1'])
+
+    raise ValueError("We have nothing here!")
+
+
+if __name__ == "__main__":
+    df = pd.DataFrame(data={'col1': [1, 2], 'col2': [3, 4]})
+    print(do_something(df))
